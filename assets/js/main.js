8c1530ccf55d96800c9c8544140d3f50f05366cc
kawsarjama <kawsarjaamac37@gmail.com>
2022-06-26 06:43:23 -0700

    all done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,25 @@
 // *** Write your code here***
+let displayResult = document.getElementById("displayResult")
+
+function display(number){
+  displayResult.value +=number
+}
+
+function compute(){
+  try{
+
+    displayResult.value = eval(displayResult.value)
+  }
+  catch(err) 
+  {
+    alert("error")
+  }
+  
+}
+function Clear(){
+  displayResult.value = null
+ 
+}
+function DELETE(){
+  displayResult.value =  displayResult.value.slice(0,-1)
+}
